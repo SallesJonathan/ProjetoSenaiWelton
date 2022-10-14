e27c9ade361ec2a3f3609b0be0185627aea0c2c4
Welton Castoldi <weltoncastoldi@hotmail.com>
2022-10-14 20:56:02 -0300

    Validadores

--- View/AppModelo.View.Windows/Cadastros/frmCadastroFuncionario.cs
+++ View/AppModelo.View.Windows/Cadastros/frmCadastroFuncionario.cs
@@ -1,5 +1,7 @@
 ﻿using AppModelo.Controller.External;
 using AppModelo.View.Windows.Helpers;
+using System;
+using System.ComponentModel;
 using System.Windows.Forms;
 
 namespace AppModelo.View.Windows.Cadastros
@@ -12,7 +14,7 @@ namespace AppModelo.View.Windows.Cadastros
             Componentes.FormatarCamposObrigatorios(this);
         }
 
-        private void btnPesquisarCep_Click(object sender, System.EventArgs e)
+        private void btnPesquisarCep_Click(object sender, EventArgs e)
         {
             //Crio a instancia do Controllador
             var cepController = new ViaCepController();
@@ -24,6 +26,30 @@ namespace AppModelo.View.Windows.Cadastros
             txtEnderecoLogradouro.Text = endereco.Logradouro;
             txtEnderecoMunicipio.Text = endereco.Localidade;
             txtEnderecoUf.Text = endereco.Uf;
+        }
+
+        private void txtNome_Validating(object sender, CancelEventArgs e)
+        {
+            //primeira regra nome < que 6 letras
+            if(txtNome.Text.Length < 6)
+            {
+                errorProvider.SetError(txtNome,"Digite seu nome completo");
+                return;
+            }
+
+            //verifica se digitou algum numero
+            foreach(var letra in txtNome.Text)
+            {
+                if (char.IsNumber(letra))
+                {
+                    errorProvider
+                        .SetError(txtNome, "Seu nome parece estar errado");
+                    return;
+                }
+            }
+            errorProvider.Clear();
+
+           
         }
     }
 }
